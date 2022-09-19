df7b791634074c7199f367f782db15f2d4118df2
Jingshi Liu <jingshiliu07@gmail.com>
2022-09-19 19:48:31 -0400

    finished the basic functions

--- src/App.js
+++ src/App.js
@@ -3,65 +3,113 @@ import ReactDOM from 'react-dom/client'
 import './App.css'
 
 const memeAPI = "https://api.imgflip.com/get_memes"
+const axios = require('axios')
 
-function getInput(id){
-  return document.querySelector(`#${id}`).value
+function Inputs({ setLineOne, setLineTwo, setTextColor }) {
+
+    function getInput(id) {
+        return document.querySelector(`#${id}`).value
+    }
+
+    return (
+        <form className='row'>
+            <label className='col'>Line one: </label>
+            <input className='form-control col' id='lineOne' type="text"
+                onChange={
+                    () => {
+                        setLineOne(getInput('lineOne'))
+                    }
+                }
+            />
+            <div></div>
+            <label className='col'>Line two:</label>
+            <input className='form-control col' id='lineTwo' type="text"
+                onChange={
+                    () => {
+                        setLineTwo(getInput('lineTwo'))
+                    }
+                }
+            />
+            <input type="color" id='colorInput' onChange={
+                () => {
+                    setTextColor(getInput('colorInput'))
+                }
+            }></input>
+        </form>
+    )
+}
+
+
+function Meme(props) {
+    console.log(props)
+    const {textColor, memeImage, lineOne, lineTwo} = props
+    return (
+        <div className='meme-image-container'>
+            {/* <span>{lineOne}</span> */}
+            <span
+                style={{color: textColor}}
+            >{lineOne}</span>
+            <img className='meme-image' src={memeImage}></img>
+            {/* <span>{lineTwo}</span> */}
+            <span style={{color: textColor}}>{lineTwo}</span>
+        </div>
+    )
 }
 
 function App() {
-  const [lineOne, setLineOne] = useState('')
-  const [lineTwo, setLineTwo] = useState('')
-  const [textColor, setTextColor] = useState('')
+    const [lineOne, setLineOne] = useState('')
+    const [lineTwo, setLineTwo] = useState('')
+    const [memeImage, setMemeImage] = useState('https://i.imgflip.com/1g8my4.jpg')
+    const [textColor, setTextColor] = useState('')
+    const [memesArray, setMemesArray] = useState([])
 
-  const textColorStyling = {
-    color: textColor,
-  }
+    const textColorStyling = {
+        color: textColor,
+    }
+
+    const inputComponentProps = {
+        setLineOne, setLineTwo, setTextColor
+    }
+
+    const memeProps = {
+        memeImage, textColor, lineOne, lineTwo
+    }
+
+    useEffect(() => {
+        requestMemes()
+    }, [])
 
 
-  return (
-    <div className="container">
-      <nav>
-        <span>Meme Generator</span>
-        <span>A React Project</span>
-      </nav>
 
-      <form className='row'>
-        <label className='col'>Line one: </label>
-        <input className='form-control col' id='lineOne' type="text" 
-          onChange={
-            ()=>{
-              setLineOne(getInput('lineOne'))
-            }
-          }
-        />
-        <div></div>
-        <label className='col'>Line two:</label>
-        <input className='form-control col' id='lineTwo' type="text" 
-          onChange={
-            ()=>{
-              setLineTwo(getInput('lineTwo'))
-            }
-          }
-        />
-        <input type="color" id='colorInput' onChange={
-          ()=>{
-            console.log(getInput('colorInput'))
-            setTextColor(getInput('colorInput'))
-          }
-        }></input>
-      </form>
 
-      <div className='meme-image-container'>
-        {/* <span>{lineOne}</span> */}
-        <span
-          style={textColorStyling}
-        >{1}</span>
-        <img className='meme-image' src='https://i.imgflip.com/1g8my4.jpg'></img>
-        {/* <span>{lineTwo}</span> */}
-        <span>{1}</span>
-      </div>
-    </div>
-  );
+    async function requestMemes() {
+        const res = await axios.get(memeAPI)
+        const data = res.data.data.memes
+        setMemesArray(data)
+    }
+
+    function getRandomMemeUrl() {
+        return memesArray[Math.floor(Math.random() * 100)].url
+    }
+
+    return (
+        <div className="container">
+            <nav>
+                <span>Meme Generator</span>
+                <span>A React Project</span>
+            </nav>
+
+            <Inputs {...inputComponentProps} />
+
+            <button className='btn btn-primary'
+                onClick={()=>{
+                    setMemeImage(getRandomMemeUrl())
+                }}
+            >Next meme</button>
+
+            <Meme {...memeProps} />
+        </div>
+    );
 }
 
 export default App;
